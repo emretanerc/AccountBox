e0ce7dfecd9148754da05b16bd99e6b500ec681e
emre taner çetinkaya <etcmobileapps@gmail.com>
2022-10-11 13:21:44 +0300

    added bottom navigation bor and material toolbar.

--- app/src/main/java/com/etcmobileapps/ibanbankaccountanddigitalaccount/view/MainViewModel.kt
+++ app/src/main/java/com/etcmobileapps/ibanbankaccountanddigitalaccount/view/MainViewModel.kt
@@ -1,30 +1,42 @@
 package com.etcmobileapps.ibanbankaccountanddigitalaccount.view
 
 import android.app.Application
-import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.etcmobileapps.ibanbankaccountanddigitalaccount.databinding.ActivityMainBinding
+import com.etcmobileapps.ibanbankaccountanddigitalaccount.model.Account
 import com.etcmobileapps.ibanbankaccountanddigitalaccount.model.Db
-import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private lateinit var database: Db
 
 
 
 
-    val context = getApplication<Application>().applicationContext!!
-    val database : Db = Db.getDatabase(context)!!
 
-    fun refreshData(){
-
+    fun initDatase() {
+        database = Db.getDatabase(getApplication())!!
     }
 
-    fun add() {
 
+     fun add(appNameValue:String,userNameValue:String,passwordValue:String) {
+        if(appNameValue != "" || userNameValue != "" || passwordValue != ""){
+            CoroutineScope(Dispatchers.IO).launch {
+                database.getAccountDao().addAccount(Account(1,appNameValue,userNameValue,passwordValue))
+            }
+            Toast.makeText(getApplication(), "Eklendi!", Toast.LENGTH_LONG).show()
+            println("eklendi")
+        } else {
+            Toast.makeText(getApplication(), "Boş alan mevcut!", Toast.LENGTH_LONG).show()
+        }
     }
 }
+
+
+
+
 
