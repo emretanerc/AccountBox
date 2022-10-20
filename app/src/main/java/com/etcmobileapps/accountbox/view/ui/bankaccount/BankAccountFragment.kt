3a1784b707066aa6f6509001dc8d7aebc9a85792
Emre Taner Çetinkaya <etcmobileapps@gmail.com>
2022-10-21 00:20:59 +0300

    Update BankAccountFragment.kt

--- app/src/main/java/com/etcmobileapps/accountbox/view/ui/bankaccount/BankAccountFragment.kt
+++ app/src/main/java/com/etcmobileapps/accountbox/view/ui/bankaccount/BankAccountFragment.kt
@@ -12,44 +12,31 @@ import com.etcmobileapps.accountbox.data.Iban
 import com.etcmobileapps.accountbox.view.adapter.BankAdapter
 import com.etcmobileapps.accountbox.databinding.FragmentBankAccountBinding
 
-
 class BankAccountFragment : Fragment() {
     private lateinit var bindingBank: FragmentBankAccountBinding
     private lateinit var viewModel: BankAccountViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[BankAccountViewModel::class.java]
-
     }
-
+ 
     override  fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         bindingBank = FragmentBankAccountBinding.inflate(inflater, container, false)
-
         viewModel.initDatabase()
-
-
-
         setupRecylerView()
-
-
-
-
         return bindingBank.root
-
     }
 
 
       fun setupRecylerView() {
-
               var data:MutableList<Iban> =  viewModel.getAllIbans()
               Log.d("data:",data.toString())
               bindingBank.bankRecylerView.layoutManager = LinearLayoutManager(requireContext())
               bindingBank.bankRecylerView.adapter = BankAdapter(data)
-
       }
 }
 
